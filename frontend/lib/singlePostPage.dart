7a5cc826cf2ffa8e5abc4b5024ebe3677c75c004
Yiqian Zhao <yzhao714@gatech.edu>
2023-01-29 15:37:07 -0500

    post box style

--- frontend/lib/singlePostPage.dart
+++ frontend/lib/singlePostPage.dart
@@ -9,13 +9,45 @@ class SinglePostPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('aaa'),
+        title:  Text("aaaa"),
       ),
       body: Column(
-        children: [
-          Text(title),
-          Text(body)
-        ],
+       
+        children:[
+           Padding(padding: const EdgeInsets.all(13.0),
+          child: Container(
+            width: 600.0,
+            height:100.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24.0),
+              color: const Color(0xFFFF9000).withOpacity(0.8),
+              ),
+              child: Center(
+                child: Text(title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )
+          ),
+        ),
+          
+          Padding(
+          padding: const EdgeInsets.all(13.0),
+         
+          child: Container(
+            width: 600.0,
+            height:650.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24.0),
+              color: const Color(0xFFFF9000).withOpacity(0.5),
+              ),
+             child: Center(
+                child: Text(body,
+                  style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+             ),
+          ),
+        ),
+        ]
       )
     );
   }
